ea35038a47e3e7c4ac58b55c38fab8ab79af6650
Diego Lucchelli <lucca86@gmail.com>
2020-07-27 17:38:52 -0300

    docs agregado

---
+++ docs/precache-manifest.60f1cfe6388678e677374f0f84eb1cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ea1ebc14021ee3a1dcda85859079f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f7bfb59a8a98464b1d8",
+    "url": "/static/css/main.df72fe04.chunk.css"
+  },
+  {
+    "revision": "2c7275a2411ef8944e9b",
+    "url": "/static/js/2.5163bc94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5163bc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7bfb59a8a98464b1d8",
+    "url": "/static/js/main.7023283e.chunk.js"
+  },
+  {
+    "revision": "4d3c5aad2ee9da455662",
+    "url": "/static/js/runtime-main.a4f11275.js"
+  }
+]);
